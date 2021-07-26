17055823829250c63a891ac6c330b3273149137e
m2vi <iuvo@gmx.at>
2021-07-26 12:18:24 +0200

    final

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const settings = {
+  delay: 0,
+  url: 'https://www.youtube.com/youtubei/v1/log_event?alt=json&key=%random%',
+};
+
+const activate = () => {
+  setInterval(async () => {
+    const random = (Math.random() * 10 * Math.PI).toString();
+    const el = document.getElementById('requests');
+    const url = settings.url.replace('%random%', random);
+
+    try {
+      await fetch(url);
+    } catch (err) {
+      const requests = parseInt(el.innerHTML);
+      el.innerHTML = requests + 1;
+    }
+  }, 0);
+};
