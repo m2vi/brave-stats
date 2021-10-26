c0b8f1d8c2b242be8b5ff2d907a2f7e40829c379
m2vi <iuvo@gmx.at>
2021-10-26 11:41:17 +0200

    lol

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 const settings = {
   delay: 0,
-  url: 'https://www.youtube.com/youtubei/v1/log_event?alt=json&key=%random%',
+  url: 'https://www.youtube.com/youtubei/v1/log_event?alt=json&key=%replace%',
 };
 
 const activate = () => {
-  setInterval(async () => {
-    const random = (Math.random() * 10 * Math.PI).toString();
+  setInterval(() => {
+    const random = (Math.random() * 1000).toString();
     const el = document.getElementById('requests');
-    const url = settings.url.replace('%random%', random);
+    const url = settings.url.replace('%replace%', random);
 
     try {
-      await fetch(url);
-    } catch (err) {
+      fetch(url);
       const requests = parseInt(el.innerHTML);
       el.innerText = requests + 1;
-    }
+    } catch (err) {}
   }, settings.delay);
 };
